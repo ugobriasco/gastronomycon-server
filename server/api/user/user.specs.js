3c9c9a62ebfd1d4448d9cdc6f43d717f0be4df89
Ugo Briasco <ugo.briasco@gmail.com>
2016-12-22 21:22:14 +0100

    Revert "unit tests"
    This reverts commit 2e9567b939014c16c46ab5dc8786592adcd72e5e.

--- server/api/user/user.specs.js
+++
@@ -1,9 +0,0 @@
-// var expect = require('chai').expect;
-// var userController = require ('./user.controller');
-
-// describe("Listing all users", function(){
-// 	describe("get all users"), function(){
-		
-// 	}
-
-// });
