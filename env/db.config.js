2e9567b939014c16c46ab5dc8786592adcd72e5e
Ugo Briasco <ugo.briasco@gmail.com>
2016-12-22 21:21:49 +0100

    unit tests

---
+++ env/db.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+	localdb:{
+		name: "local database",
+		url : "mongodb://localhost/grocerybot",
+		port: 27017
+
+	}
+
+	// looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
---
+++ server/api/user/user.specs.js
@@ -0,0 +1,9 @@
+// var expect = require('chai').expect;
+// var userController = require ('./user.controller');
+
+// describe("Listing all users", function(){
+// 	describe("get all users"), function(){
+		
+// 	}
+
+// });
