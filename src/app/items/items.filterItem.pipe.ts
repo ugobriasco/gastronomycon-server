e3a0b6045b3cde02321f93d8be27a02db6195da0
Ugo Briasco <ugo.briasco@gmail.com>
2017-01-22 21:21:16 +0100

    search filter upgrade

--- src/app/items/items.filterItem.pipe.ts
+++ src/app/items/items.filterItem.pipe.ts
@@ -7,10 +7,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
 	transform(items: any[], args: string): any {
+
+		let low_args = args.toLowerCase();
        if(!args){
        	return items; //return all if no entry
        } else {
-       return items.filter(item => item.name.it.main.toLowerCase().indexOf(args[0]) !== -1);
+       return items.filter(item => 
+       	item.name.it.main.toLowerCase().indexOf(low_args[0]) !== -1 || 
+       	item.name.it.spec.toLowerCase().indexOf(low_args[0]) !== -1 || 
+       	item.name.de.main.toLowerCase().indexOf(low_args[0]) !== -1 || 
+       	item.name.de.spec.toLowerCase().indexOf(low_args[0]) !== -1 || 
+       	item.name.pl.main.toLowerCase().indexOf(low_args[0]) !== -1 || 
+       	item.name.pl.spec.toLowerCase().indexOf(low_args[0]) !== -1  
+
+       	);
 
        }
     }	
