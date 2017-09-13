a0ac9dc6e5d171b0127be788430931154f7479a5
Ugo Briasco <ugo.briasco@gmail.com>
2017-09-13 16:04:04 +0200

    bugfix - handling inconsistent user id by searching

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -21,22 +21,33 @@ exports.getAllUsers = function(req,res){
 }
 
 exports.getUser = function(req, res) {
-    User.findById(req.params.objID, (err, user) => {
-        if(err) res.send(err);
-        res.json({
-            data: user
+    if (req.params.objID.match(/^[0-9a-fA-F]{24}$/)) {
+        User.findById(req.params.objID, (err, user) => {
+            if(err) res.status(404).send(err);
+            if(!user) res.status(404).send({message: 'No user found'});
+            else res.json({data: user});
+            
         });
-    });     
+    } else {
+       res.status(404).send({message: 'No user no user found'});
+    }
+         
 }
 
 exports.updateUser = function(req, res) {
-    User.findById({_id: req.params.objID}, (err, user) => {
-        if(err) res.send(err);
-        Object.assign(user, req.body).save((err, user) => {
-            if(err) res.send(err);
-            res.json({ message: 'User updated!', user });
-        }); 
-    });
+    if(req.params.objID.match(/^[0-9a-fA-F]{24}$/)){
+         User.findById(req.params.objID, (err, user) => {
+            if(err) res.status(404).send(err);
+            if(!user) res.status(404).send({message: 'No user found'});
+            Object.assign(user, req.body).save((err, user) => {
+                if(err) res.send(err);
+                res.json({ message: 'User updated!', user });
+            }); 
+        });
+    } else {
+        res.status(404).send({message: 'No user no user found'});
+    }
+   
 }
 
 
