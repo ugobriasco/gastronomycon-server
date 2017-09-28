e6703d902fa435f4b22478fac18b27aacd3a9313
Ugo Briasco <ugo.briasco@gmail.com>
2017-09-28 23:08:25 +0200

    input validation

--- server/list/list.controller.js
+++ server/list/list.controller.js
@@ -1,8 +1,5 @@
 const List = require('./list.model');
 const User = require('../user/user.model');
-//members:
-//	allowedUsers: Array,
-//	items: Array
 
 
 //Create a new list
@@ -20,15 +17,23 @@ exports.postList = (req, res) => {
 	: message = 'empty list created';
 
 	list.save(function(err){
-		if(err) throw err;
+		if(err) res.status(500).send(err);
 		res.json({message: message, data: list});
 	});
+}
+
+// get a list given its _id
+exports.getList = (req, res) => {
+	List.findById(req.params.objID, function(list, err){
+		if(err) res.status(500).send(err);
+		res.json({data: list});
+	})
 }
 
 //query all list saved (admin function)
 exports.getAllLists = (req, res) => {
 	List.find(function(err, lists){
-		if(err) throw err;
+		if(err) res.status(500).send(err);
 		res.json({data: lists});
 	});
 }
@@ -36,67 +41,53 @@ exports.getAllLists = (req, res) => {
 //deleta a given list
 exports.deleteList = (req, res) => {
 	List.findByIdAndRemove(req.params.objID, function(err){
-		if(err) throw err;
+		if(err) res.status(500).send(err);
 		res.json({message: 'List '+req.params.objID+' removed'})
 	});
 }
 
 //update the items in a list
 exports.replaceListItems = (req, res) => {
-	if(req.params.objID.match(/^[0-9a-fA-F]{24}$/)){
-		List.findById(req.params.objID,(err, list) => {
-			if(err) res.status(404).send(err);
-			if(!list) res.status(404).send({message: 'No list found'});
-			list.items.splice(0,list.items.length, req.body);
-			Object.assign(list, req.body).save((err, list) => {
-				if(err) res.send(err);
-				res.json({ message: 'List updated!', list });
-			});
+	List.findById(req.params.objID,(err, list) => {
+		if(err) res.status(500).send(err);
+		if(!list) res.status(404).send({message: 'No list found'});
+		list.items.splice(0,list.items.length, req.body);
+		Object.assign(list, req.body.data).save((err, list) => {
+			if(err) res.status(500).send(err);
+			res.json({ message: 'List updated!', list });
 		});
-	} else {
-		res.status(404).send({message: 'No list found'});
-	}
+	});
 }
 
-//share a list 
-exports.shareList = (req, res) => {
+// //share a list 
+	// exports.shareList = (req, res) => {
 
-	if(req.params.objID.match(/^[0-9a-fA-F]{24}$/)){
-
-		List.findById(req.params.objID,(err, list) => {
-			if(err) res.status(404).send(err);
-			if(!list) res.status(404).send({message: 'No list found'});
-			if(!req.body.userID) res.status(422).send({message: 'No userID provided'});
-			//if(!req.body.userID.match(/^[0-9a-fA-F]{24}$/)) res.status(422).send({message: 'No user founf'});
+	// 	List.findById(req.params.objID,(err, list) => {
+	// 		if(err) res.status(404).send(err);
+	// 		if(!list) res.status(404).send({message: 'No list found'});
+	// 		if(!req.body.userID) res.status(422).send({message: 'No userID provided'});
+	// 		//if(!req.body.userID.match(/^[0-9a-fA-F]{24}$/)) res.status(422).send({message: 'No user founf'});
 
 			
 
-			// User.findById(req.body.userID, (err, res, next) =>{
-			// 	if(err) res.status(404).send(err);
-			// 	else list.allowedUsers.push(res._id);
-			// 	next();
-			// });
+	// 		// User.findById(req.body.userID, (err, res, next) =>{
+	// 		// 	if(err) res.status(404).send(err);
+	// 		// 	else list.allowedUsers.push(res._id);
+	// 		// 	next();
+	// 		// });
 
-			list.allowedUsers.push(req.body.userID);
+	// 		list.allowedUsers.push(req.body.userID);
 
 
-			Object.assign(list, req.body).save((err, list) => {
-				if(err) res.send(err);
-				res.json({ message: 'List updated!', list });
-			});
+	// 		Object.assign(list, req.body).save((err, list) => {
+	// 			if(err) res.send(err);
+	// 			res.json({ message: 'List updated!', list });
+	// 		});
 
-		});
-	} else {
-		res.status(404).send({message: 'No list found'});
-	}
+	// 	});
+	// }
 
-}
-
-exports.unshareList = (req, res) => {
+// exports.unshareList = (req, res) => {
 	
-}
+// }
 
-
-validateID = function(id){
-	return id.match(/^[0-9a-fA-F]{24}$/)
-}
---
+++ server/list/list.validation.js
@@ -0,0 +1,52 @@
+const Joi = require('joi');
+
+
+
+module.exports = {
+	
+	postList:{
+		body:{
+			ownerID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required(),
+			data: Joi.array()
+		}
+	},
+
+	getList:{
+		params:{
+			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+		}
+		
+	},
+
+	deleteList:{
+		params:{
+			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+		}
+
+	},
+
+	replaceListItem:{
+		params:{
+			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+		},
+		body:{
+			data: Joi.array()
+		}
+	}
+	shareList:{
+		params:{
+			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+		}
+	}
+
+	unshareList:{
+		params:{
+			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
+		}
+	}
+
+}
+
+
+
+
