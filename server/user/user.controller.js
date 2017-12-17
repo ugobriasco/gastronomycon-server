d84886da1a622e87428d8e33995335462ce9179d
Ugo Briasco <ugo.briasco@gmail.com>
2017-12-17 01:08:45 +0100

    Lint user controller

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -1,54 +1,42 @@
-const User = require('./user.model');
+const User = require("./user.model");
 
-exports.postUser = function(req, res){
-	// var user = new User({
-	// 	email: req.body.email,
-	// 	password: req.body.password
-	// });
+const postUser = (req, res) => {
+  res.status(401).json({ meassge: "This route is deprecated" });
+};
 
-	// user.save(function(err){
-	// 	if(err) throw err;
-	// 	res.json({message: "new user"});
-	// });
-    res.status(401).json({meassge: "This route is deprecated"});
-}
+const getAllUsers = (req, res) => {
+  User.find((err, users) => {
+    if (err) res.status(500).send(err);
+    res.json({ data: users });
+  });
+};
 
-exports.getAllUsers = function(req,res){
-	User.find(function(err, users){
-		if(err) res.status(500).send(err);
-		res.json({data: users});
-	});
-}
+const getUser = (req, res) => {
+  User.findById(req.params.userID, (err, user) => {
+    if (err) res.status(500).send(err);
+    if (!user) res.status(404).send({ message: "No user found" });
+    res.json({ data: user });
+  });
+};
 
-exports.getUser = function(req, res) {
-    User.findById(req.params.userID, (err, user) => {
-        if(err) res.status(500).send(err);
-        if(!user) res.status(404).send({message: 'No user found'});
-        else res.json({data: user});
-        
+const updateUser = (req, res) => {
+  User.findById(req.params.userID, (err, user) => {
+    if (err) res.status(500).send(err);
+    if (!user) res.status(404).send({ message: "No user found" });
+    Object.assign(user, req.body).save((err, user) => {
+      if (err) res.status(500).send(err);
+      res.json({ message: "User updated!", user });
     });
-}
+  });
+};
 
-//to refactor: a put can modify everything
-exports.updateUser = function(req, res) {
-    User.findById(req.params.userID, (err, user) => {
-        if(err) res.status(500).send(err);
-        if(!user) res.status(404).send({message: 'No user found'});
-        Object.assign(user, req.body).save((err, user) => {
-            if(err) res.status(500).send(err);
-            res.json({ message: 'User updated!', user });
-        }); 
-    });   
-}
+const deleteUser = (req, res) => {
+  const userID = req.params.userID;
+  User.findByIdAndRemove(userID, (err, user) => {
+    if (err) res.status(500).send(err);
+    if (!user) res.status(404).send({ message: "No user found" });
+    res.json("user: " + userID + " removed");
+  });
+};
 
-exports.deleteUser = function(req, res) {
-    const userID = req.params.userID;
-
-    User.findByIdAndRemove(userID, function(err, user){
-        if(err) res.status(500).send(err);
-        if(!user) res.status(404).send({message: 'No user found'});
-        else res.json("user: " +userID +' removed');
-    });
-}
-
-
+module.exports = { postUser, getUser, updateUser, deleteUser };
