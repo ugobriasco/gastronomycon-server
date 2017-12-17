f0dbbb48e64b87cf226a1193369237ca2ee29d3e
Ugo Briasco <ugo.briasco@gmail.com>
2017-12-17 21:42:01 +0100

    Fixup! - restore trashed file

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'prettier',
+  globals: {
+    describe: true,
+    beforeEach: true,
+    it: true,
+    test: true,
+    expect: true,
+    should: true,
+    afterEach: true
+  }
+};
