82095a014af2c8ad9149a20a9309ca782873f6e9
Ugo Briasco <ugo.briasco@sumup.com>
2017-12-17 21:46:12 +0100

    Refactor user component +
    * cleanup user component
    
    * lint user model
    
    * Revert "lint user model"
    
    This reverts commit 3151914238b99ef60b042998606a8e0407d0ff40.
    
    * Add unit tests to user model
    
    * Fixup! - restore trashed file

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -1,51 +1,43 @@
-var mongoose = require('mongoose'),
-	bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+    facebook: String,
+    tokens: Array,
+    profile: {
+      name: String,
+      avatar: String,
+      cover: String
+    },
+    role: {
+      type: String,
+      enum: ['User', 'Admin'],
+      default: 'User'
+    }
+  },
+  {
+    timestamps: true
+  }
+);
 
-var UserSchema = new mongoose.Schema({
-	email: {type: String, unique: true},
-	password: String,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-
-  facebook: String,
-  tokens: Array,
-
-
-  profile: {
-    name: String,
-    avatar: String,
-    cover: String,
-  	},
-	role: {
-  	type: String,
-  	enum: ['User', 'Admin'],
-  	default: 'User'
-	},
-//	resetPasswordToken: {type: String},
-//	resetPasswordExpires: {type: Date},
-},{timestamps: true});
-
-
-/**
- * Password hash middleware.
- */
-
-
-
-UserSchema.pre('save', function(callback) {
-  var user = this;
-  if (!user.isModified('password')) return callback();
+// Password hash middleware.
+UserSchema.pre('save', done => {
+  const user = this;
+  if (!user.isModified('password')) return done();
   bcrypt.genSalt(5, function(err, salt) {
-    if (err) return callback(err);
+    if (err) return done(err);
     bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) return callback(err);
+      if (err) return done(err);
       user.password = hash;
-      callback();
+      done();
     });
   });
 });
-
 
 UserSchema.methods.verifyPassword = function(password, cb) {
   bcrypt.compare(password, this.password, function(err, isMatch) {
@@ -64,7 +56,10 @@ UserSchema.methods.gravatar = function gravatar(size) {
   if (!this.email) {
     return `https://gravatar.com/avatar/?s=${size}&d=retro`;
   }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
+  const md5 = crypto
+    .createHash('md5')
+    .update(this.email)
+    .digest('hex');
   return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
 };
 
--- server/user/user.validation.js
+++ server/user/user.validation.js
@@ -1,25 +1,25 @@
 const Joi = require('joi');
 
 module.exports = {
-
-	
-	getUser:{
-		params:{
-			userID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
-		}
-	},
-
-	updateUser:{
-		params:{
-			userID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
-		}
-	},
-
-	deleteUser:{
-		params:{
-			userID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
-		}
-
-	}
-
-}
+  getUser: {
+    params: {
+      userID: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required()
+    }
+  },
+  updateUser: {
+    params: {
+      userID: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required()
+    }
+  },
+  deleteUser: {
+    params: {
+      userID: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required()
+    }
+  }
+};
