dedcc962ad9bf60da3f38cb5cdadc9c5fb552689
Ugo Briasco <ugo.briasco@gmail.com>
2017-12-23 11:54:19 +0100

    Linting workspace

--- server/item/item.controller.js
+++ server/item/item.controller.js
@@ -1,20 +1,18 @@
 const Item = require('./item.model');
 
-
 //endpoint for api/items
 
-exports.postItem = function(req, res){
-	var item = new Item();
-	item.type = req.body.type;
-	item.pic = req.body.pic;
-	item.name = req.body.name;
+const postItem = function(req, res) {
+  var item = new Item();
+  item.type = req.body.type;
+  item.pic = req.body.pic;
+  item.name = req.body.name;
 
-	item.save(function(err){
-		if(err) res.status(500).send(err);
-		res.json({message: 'item added', data: item});
-
-	});
-}
+  item.save(function(err) {
+    if (err) res.status(500).send(err);
+    res.json({ message: 'item added', data: item });
+  });
+};
 
 //deprecated
 // getAllItems = function(req, res){
@@ -25,53 +23,46 @@ exports.postItem = function(req, res){
 // 	});
 // }
 
-exports.queryItems = function(req, res){
+const queryItems = function(req, res) {
+  if (req.query.id) {
+    //new routes
+    res.status(501).send('route under construction');
+  } else {
+    //backcompatibility
+    Item.find(function(err, items) {
+      if (err) res.status(500).send(err);
+      res.json({ data: items });
+    });
+  }
+};
 
-	if(req.query.id){//new routes
-		res.status(501).send('route under construction'); 
-	} else { //backcompatibility
-		Item.find(function(err, items){
-			if(err) res.status(500).send(err);
-			res.json({data: items});
-		});
-	}
+const getItem = function(req, res) {
+  Item.findById(req.params.objID, function(err, item) {
+    if (err) res.status(500).send(err);
+    res.json({ data: item });
+  });
+};
 
-}
+const deleteItem = function(req, res) {
+  Item.findByIdAndRemove(req.params.objID, function(err) {
+    if (err) res.status(500).send(err);
+    res.json({ message: 'Item ' + req.params.objID + ' removed' });
+  });
+};
 
-exports.getItem = function(req, res){
-	Item.findById(req.params.objID, function(err, item){
-		if(err) res.status(500).send(err);
-		res.json({data: item});
+const updateItem = function(req, res) {
+  Item.findById(req.params.objID, function(err, item) {
+    if (err) res.status(500).send(err);
 
-	});
-}
+    item.type = req.body.type;
+    item.pic = req.body.pic;
+    item.name = req.body.name;
 
+    item.save(function(err) {
+      if (err) res.status(500).send(err);
+      res.json(item);
+    });
+  });
+};
 
-exports.deleteItem = function(req, res){
-	Item.findByIdAndRemove(req.params.objID, function(err){
-		if(err) res.status(500).send(err);
-		res.json({message: 'Item '+req.params.objID+' removed'})
-
-	});
-}
-
-exports.updateItem = function(req, res){
-	Item.findById(req.params.objID, function(err, item){
-		if(err) res.status(500).send(err);
-
-		item.type = req.body.type;
-		item.pic = req.body.pic;
-		item.name = req.body.name;
-
-		item.save(function(err){
-			if(err) res.status(500).send(err);
-			res.json(item);
-
-		});
-	});
-}
-
-
-
-
-
+module.exports = { postItem, queryItems, getItem, deleteItem, updateItem };
--- server/item/item.validation.js
+++ server/item/item.validation.js
@@ -1,53 +1,51 @@
 const Joi = require('joi');
-const regex = require('regex-collection');
-
+const regex_weburl = require('./regex-weburl');
 
 module.exports = {
-	
-	getItem:{
-		params:{
-			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
-		}
-	},
-	postItem:{
-		body:{
-			type: Joi.string(),
-			pic: Joi.string().regex(require('regex-collection'), 'regex_weburl'),
-			name: Joi.string(),
-		}
+  getItem: {
+    params: {
+      objID: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required()
+    }
+  },
+  postItem: {
+    body: {
+      type: Joi.string(),
+      pic: Joi.string().regex(regex_weburl),
+      name: Joi.string()
+    }
+  },
+  queryItems: {
+    query: Joi.any()
+  },
 
-	},
-	queryItems:{
-		query: Joi.any()
-	},
+  getItem: {
+    params: {
+      objID: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required()
+    }
+  },
 
-	getItem:{
-		params:{
-			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
-		}
-		
-	},
+  deleteItem: {
+    params: {
+      objID: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required()
+    }
+  },
 
-	deleteItem:{
-		params:{
-			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
-		}
-
-	},
-
-	updateItem:{
-		params:{
-			objID: Joi.string().regex(/^[0-9a-fA-F]{24}$/).required()
-		},
-		body:{
-			type: Joi.string(),
-			pic: Joi.string().regex(require('regex-collection'), 'regex_weburl'),
-			name: Joi.string()
-		}
-	},
-
-}
-
-
-
-
+  updateItem: {
+    params: {
+      objID: Joi.string()
+        .regex(/^[0-9a-fA-F]{24}$/)
+        .required()
+    },
+    body: {
+      type: Joi.string(),
+      pic: Joi.string().regex(regex_weburl),
+      name: Joi.string()
+    }
+  }
+};
