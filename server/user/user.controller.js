26d23b6024edce36711a2560269e7506ae5bf186
Ugo Briasco <ugo.briasco@sumup.com>
2017-12-27 11:25:43 +0100

    Refactor/auth
    * Solve missing backcompatibility jsonwebtoken v8
    * Fix - DeprecationWarning:  is deprecated in mongoose >= 4.11.0, use  instead #5399
    * Refactor auth and user component

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -1,7 +1,7 @@
-const User = require("./user.model");
+const User = require('./user.model');
 
 const postUser = (req, res) => {
-  res.status(401).json({ meassge: "This route is deprecated" });
+  res.status(401).json({ meassge: 'This route is deprecated' });
 };
 
 const getAllUsers = (req, res) => {
@@ -13,19 +13,19 @@ const getAllUsers = (req, res) => {
 
 const getUser = (req, res) => {
   User.findById(req.params.userID, (err, user) => {
-    if (err) res.status(500).send(err);
-    if (!user) res.status(404).send({ message: "No user found" });
-    res.json({ data: user });
+    if (err) return res.status(500).send(err);
+    if (!user) return res.status(404).send({ message: 'No user found' });
+    return res.json({ data: user });
   });
 };
 
 const updateUser = (req, res) => {
   User.findById(req.params.userID, (err, user) => {
     if (err) res.status(500).send(err);
-    if (!user) res.status(404).send({ message: "No user found" });
+    if (!user) res.status(404).send({ message: 'No user found' });
     Object.assign(user, req.body).save((err, user) => {
       if (err) res.status(500).send(err);
-      res.json({ message: "User updated!", user });
+      res.json({ message: 'User updated!', user });
     });
   });
 };
@@ -33,10 +33,10 @@ const updateUser = (req, res) => {
 const deleteUser = (req, res) => {
   const userID = req.params.userID;
   User.findByIdAndRemove(userID, (err, user) => {
-    if (err) res.status(500).send(err);
-    if (!user) res.status(404).send({ message: "No user found" });
-    res.json("user: " + userID + " removed");
+    if (err) return res.status(500).send(err);
+    if (!user) return res.status(404).send({ message: 'No user found' });
+    return res.json('user: ' + userID + ' removed');
   });
 };
 
-module.exports = { postUser, getUser, updateUser, deleteUser };
+module.exports = { postUser, getUser, getAllUsers, updateUser, deleteUser };
