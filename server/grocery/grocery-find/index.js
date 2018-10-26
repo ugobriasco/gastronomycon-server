b3ac2a1a33396371b295eb7bd0fdeca613c4a855
Ugo <ugo.briasco@sumup.com>
2018-10-27 00:30:29 +0200

    add translation param logic

--- server/grocery/grocery-find/index.js
+++ server/grocery/grocery-find/index.js
@@ -7,7 +7,8 @@ const {
 const queryPrimaryName = require('./query-like-primary-name');
 const queryID = require('./query-equal-id');
 
-const { pivotToLanguage } = require('./map-language-to-lcid');
+const { pivotToLanguage } = require('./lib/map-language-to-lcid');
+const mapLocale = require('./lib/map-locale');
 
 // SPECS
 // /grocery?lang=it&name=cipolla
@@ -22,7 +23,8 @@ const findGrocery = req => {
   const primaryName = req.query.primaryName;
   const name = req.query.name;
 
-  const locale = getLocale(req.query.lang);
+  const locale = mapLocale(req.query.lang);
+  const translate = getTranslationQueryString(req.query.translate);
 
   const query = buildQuery({
     _id,
@@ -32,12 +34,13 @@ const findGrocery = req => {
     name
   });
 
-  return Grocery.find(query).then(groceries =>
-    groceries.map(grocery => {
-      // TODO: Filter results by available locale
-      return grocery;
-    })
-  );
+  return Grocery.find(query)
+    .select(`-_id category primaryName ${translate}`)
+    .then(groceries =>
+      groceries.map(grocery => {
+        return grocery;
+      })
+    );
   // TODO: Filter results by selected locale. If none, skip
 };
 
@@ -49,25 +52,21 @@ function buildQuery({ category, primaryName, locale, name, _id }) {
   if (category) return queryCategory(category);
   if (category && name && locale)
     return queryCategoryAndNameAndLocale(category, name, locale);
-
   return {};
 }
 
-// maps [it, IT, it_it, it_IT, IT_it, IT_IT, it-it, it-IT, IT-it, IT-IT] to "it_IT"
-function getLocale(lang) {
-  const dashRe = new RegExp('-', 'g');
-  const re_re = new RegExp('[a-z]{2}_(?:[a-z]{2}){1,2}(?:_[a-z]{2})?$', 'i');
-  const _RE = new RegExp('_[a-z]{2}', 'i');
-  const re_ = new RegExp('[a-z]{2}_', 'i');
+function getTranslationQueryString(translateParam) {
+  console.log(translateParam);
 
-  lang = lang.replace(dashRe, '_');
-  if (lang.match(re_re)) {
-    return lang
-      .replace(_RE, x => x.toUpperCase())
-      .replace(re_, x => x.toLowerCase());
+  if (typeof translateParam === 'string') {
+    return mapLocale(translateParam);
+  }
+
+  if (typeof translateParam === 'object') {
+    const arrayOfLocale = translateParam.map(obj => mapLocale(obj));
+    return arrayOfLocale.join(' ');
   } else {
-    // case pivot language, like 'it' or IT
-    return pivotToLanguage(lang.toLowerCase());
+    return 'it_IT de_DE en_GB es_ES fr_FR';
   }
 }
 
