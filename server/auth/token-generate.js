973e48e77c3e6d047188f934ec74dbdd42a384ab
Ugo <ugo.briasco@sumup.com>
2018-12-01 15:50:21 +0100

    make toke-generate more versatile

--- server/auth/token-generate.js
+++ server/auth/token-generate.js
@@ -1,14 +1,15 @@
 const jwt = require('jsonwebtoken');
 const cfg = require('../../cfg');
 
-function generateToken(_user) {
+function generateToken(_user, _secret, _expire) {
   const user = {
     email: _user.email,
     userID: _user._id,
     role: _user.role
   };
-  return jwt.sign(user, cfg.secret, {
-    expiresIn: 10080 // in seconds
-  });
+  const secret = _secret || cfg.secret;
+  const expiresIn = _expire || 10080; // in seconds
+
+  return jwt.sign(user, secret, { expiresIn });
 }
 module.exports = generateToken;
