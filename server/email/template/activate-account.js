8027d2aeba72f242eeb1a2ff12cfc03a753deae8
Ugo <ugo.briasco@sumup.com>
2018-12-02 16:37:23 +0100

    Add html to activation mail

--- server/email/template/activate-account.js
+++ server/email/template/activate-account.js
@@ -6,11 +6,25 @@ const templateActivation = props => {
     Enjoy!
   `;
 
+  const html = `
+  <div>
+    <h1>Welcome in Gastronomycon!</h1>
+    <p>Here your activation link:</p>
+    <a href="https://${props.host || 'gcon.matchyourtie.com'}/activate/${
+    props.token
+  }">https://${props.host || 'gcon.matchyourtie.com'}/activate/${
+    props.token
+  }</a>
+    <p>Enjoy!</p>
+  </div>
+  `;
+
   return {
     from: '"Gastronomycon" <noreply@gastronomycon.matchyourtie.com>',
     to: props.to,
     subject: 'Welome to Gastronomycon!',
-    text
+    text,
+    html
   };
 };
 
