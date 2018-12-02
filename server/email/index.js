0f84ff61ed74f372b0d6449603f94c653f55dd19
Ugo <ugo.briasco@sumup.com>
2018-12-02 22:01:06 +0100

    Implement a Promise based mail delivery
    requires heavy cleanup

--- server/email/index.js
+++ server/email/index.js
@@ -1,20 +1,55 @@
 const nodemailer = require('nodemailer');
+const ejs = require('ejs');
 const cfg = require('../../cfg');
 const { templateActivation, welcome } = require('./template');
 const getTransporter = require('./get-transporter');
 
 const sendEmail = props => {
-  const { email, token } = props;
-
-  const transporter = nodemailer.createTransport(
-    getTransporter(cfg.email_service)
-  );
-
+  const { email, token, template } = props;
   const host = cfg.host;
 
-  const mailOptions = templateActivation({ to: email, token, host });
-  //const mailOptions = welcome({ to: email });
+  // Sets the service used
+  const transporter = new Promise((resolve, reject) => {
+    resolve(nodemailer.createTransport(getTransporter(cfg.email_service)));
+  });
 
+  // Send email
+  return Promise.all([
+    loadTemplate({ to: email, token, host, template }),
+    transporter
+  ]).then(promises => {
+    const email = promises[0];
+    const transporter = promises[1];
+    return send(email, transporter);
+  });
+};
+
+// build the email
+const loadTemplate = props => {
+  const { to, token, host, type } = props;
+  const DIR = getTemplate(type);
+  return ejs.renderFile(DIR, { token, host }).then(html => {
+    return {
+      from: '"Gastronomycon" <noreply@gastronomycon.matchyourtie.com>',
+      to: props.to,
+      subjcet: 'Welcome to Gastronomycon',
+      text: 'Welcome to Gastronomycon',
+      html
+    };
+  });
+};
+
+// util for selecting the specific template
+function getTemplate(template) {
+  if ((template = 'activation')) {
+    return `${__dirname}/template/welcome.ejs`;
+  }
+
+  return `${__dirname}/template/welcome.ejs`;
+}
+
+//  Send email
+const send = (mailOptions, transporter) => {
   return new Promise((resolve, reject) => {
     return transporter.sendMail(mailOptions, (err, info) => {
       if (err) {
--- server/email/template/activate-account.js
+++ server/email/template/activate-account.js
@@ -1,4 +1,4 @@
-const templateActivation = props => {
+const _templateActivation = props => {
   const text = `
     Welcome in Gastronomycon! \n
     Here your activation link: \n
@@ -26,6 +26,41 @@ const templateActivation = props => {
     text,
     html
   };
+};
+
+const templateActivation = props => {
+  return new Promise((resolve, reject) => {
+    console.log(props);
+    const text = `
+      Welcome in Gastronomycon! \n
+      Here your activation link: \n
+      ${props.host || 'https://gcon.matchyourtie.com'}/activate/${
+      props.token
+    } \n
+      Enjoy!
+    `;
+
+    const html = `
+    <div>
+      <h1>Welcome in Gastronomycon!</h1>
+      <p>Here your activation link:</p>
+      <a href="${props.host || 'https://gcon.matchyourtie.com'}/activate/${
+      props.token
+    }">${props.host || 'https://gcon.matchyourtie.com'}/activate/${
+      props.token
+    }</a>
+      <p>Enjoy!</p>
+    </div>
+    `;
+
+    resolve({
+      from: '"Gastronomycon" <noreply@gastronomycon.matchyourtie.com>',
+      to: props.to,
+      subject: 'Welome to Gastronomycon!',
+      text,
+      html
+    });
+  });
 };
 
 module.exports = templateActivation;
