355a6235b6dd263742835d53eeb4e65f086ee487
Ugo <ugo.briasco@sumup.com>
2018-12-16 14:59:56 +0100

    in the activation JWT replace dots with string, in order to be digested in GH Pages

--- server/auth/activate-account.js
+++ server/auth/activate-account.js
@@ -1,10 +1,11 @@
 const User = require('../user/user.model');
 const verifyToken = require('./token-verify');
+const generateToken = require('./token-generate');
 
 const cfg = require('../../cfg');
 
 const activateAccount = token =>
-  verifyToken({ token, secret: cfg.activation_secret }).then(res => {
+  verifyActivationToken(token).then(res => {
     if (res.status != 200) return res;
 
     return User.findOne({ email: res.decoded.email })
@@ -29,4 +30,22 @@ const activateAccount = token =>
       });
   });
 
-module.exports = activateAccount;
+const generateActivationToken = user => {
+  const token = generateToken(user, cfg.activation_secret);
+  return token.replace('.', 'TAA'); //GH Pages do not accept dots in url encoded links
+  //return token;
+};
+
+// Privates
+const verifyActivationToken = token => {
+  const compliantToken = token.replace('TAA', '.'); //Reverse mapping due to GH Pages restriction
+  return verifyToken({
+    token: compliantToken,
+    secret: cfg.activation_secret
+  }).then(res => {
+    console.log(res);
+    return res;
+  });
+};
+
+module.exports = { activateAccount, generateActivationToken };
