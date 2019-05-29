6173ff24c0c8ca359bded4eb48183d1a0e6ef8dd
Ugo <ugo.briasco@sumup.com>
2019-05-29 18:11:32 +0200

    Refactor reset password logic

--- server/api/__test__/api.v1.0.spec.js
+++ server/api/__test__/api.v1.0.spec.js
@@ -1,11 +1,11 @@
 // https://stackoverflow.com/questions/24153261/joining-tests-from-multiple-files-with-mocha-js
 
-describe.skip('API v1.0', () => {
+describe('API v1.0 Integration Tests', () => {
   after(done => {
     require('../v1.0/test/cleanup.utils.js');
     done();
   });
-  require('../v1.0/test/api.auth.login.spec.js');
-  require('../v1.0/test/api.auth.signup.spec.js');
-  require('../v1.0/test/api.user.spec.js');
+  //require('../v1.0/test/api.auth.login.spec.js');
+  //require('../v1.0/test/api.auth.signup.spec.js');
+  //require('../v1.0/test/api.user.spec.js');
 });
